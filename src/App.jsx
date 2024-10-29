8cf1da63dac5e6e5d5dcdc89cf53297060e703d2
NatanaelLimaDEV <natanaellimaofc@gmail.com>
2024-10-28 23:15:52 -0300

    add button-contato e atualizado app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import Button from "./componentes/button";
+import ButtonContato from "./componentes/button-contato";
 import Nav from "./componentes/nav";
+
+import { FaInstagram, FaRegMap, FaWhatsapp } from "react-icons/fa6";
 
 function App() {
 
@@ -17,11 +20,11 @@ function App() {
           </p>
           <Button />
         </div>
-        {/* <div className="bt-bottom">
+        <div className="bt-bottom">
           <ButtonContato text={<FaInstagram className="icon" />} />
           <ButtonContato text={<FaWhatsapp className="icon" />} />
           <ButtonContato text={<FaRegMap className="icon" />} />
-        </div> */}
+        </div>
       </div>
     </>
   );
